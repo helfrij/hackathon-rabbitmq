b84f0b397c012db8e653de95a6cc63c85f8bb488
Jessie Helfrich <jessie.helfrich@7shifts.com>
2022-03-28 13:16:47 -0600

    Updated the apps to log info-level logs.

--- services/consumer/app.py
+++ services/consumer/app.py
@@ -3,6 +3,9 @@ import logging
 from config import RABBITMQ_HOST, RABBITMQ_PORT, RABBITMQ_USER, RABBITMQ_PASSWORD
 
 
+logging.getLogger().setLevel('INFO')
+
+
 def main():
     connection = pika.BlockingConnection(pika.ConnectionParameters(
         host=RABBITMQ_HOST,
@@ -13,7 +16,7 @@ def main():
     channel.basic_qos(prefetch_count=1)
 
     def on_message(ch, method, properties, body):
-        logging.error('Received message: {message}'.format(message=body))
+        logging.info('Received message: {message}'.format(message=body))
         channel.basic_ack(method.delivery_tag)
 
     channel.queue_declare(queue='test', durable=True, exclusive=False, auto_delete=False, arguments={'x-queue-type': 'stream'})
--- services/producer/app.py
+++ services/producer/app.py
@@ -4,6 +4,9 @@ import logging
 from config import RABBITMQ_HOST, RABBITMQ_PORT, RABBITMQ_USER, RABBITMQ_PASSWORD
 
 
+logging.getLogger().setLevel('INFO')
+
+
 def main():
     connection = pika.BlockingConnection(pika.ConnectionParameters(
         host=RABBITMQ_HOST,
@@ -14,7 +17,7 @@ def main():
     channel.queue_declare(queue='test', durable=True, exclusive=False, auto_delete=False, arguments={'x-queue-type': 'stream'})
 
     while True:
-        logging.error('Sending message')
+        logging.info('Sending message')
         message = 'Hello, world'.encode('utf-8')
         channel.basic_publish(exchange='', routing_key='test', body=message)
         time.sleep(5)
